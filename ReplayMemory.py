a3a3d3c944ac1f40e49eb3ce111ea07fcba32473
Pablo del Mazo <delmazosevillanopablo@gmail.com>
2020-12-31 16:45:51 +0100

    Changes in ReplayMemory

--- ReplayMemory.py
+++ ReplayMemory.py
@@ -6,10 +6,11 @@ Transition = namedtuple('Transition',\
         ('state', 'policy', 'reward'))
 
 class ReplayMemory:
-    def __init__(self, capacity):
+    def __init__(self, capacity, start_size):
       self.capacity = capacity
       self.memory = []
       self.position = 0
+      self.REPLAY_START_SIZE = start_size
 
     def add(self, *args):
         if self.__len__() < self.capacity:
