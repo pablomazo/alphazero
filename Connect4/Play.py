09398795fecb8bec369ef590627cf215637c49d8
Pablo del Mazo <delmazosevillanopablo@gmail.com>
2020-12-31 12:04:24 +0100

    Possibility to play againts agent

--- Connect4/Play.py
+++ Connect4/Play.py
@@ -1,26 +1,63 @@
+import numpy as np
+import sys
+sys.path.append('../')
+
 import torch
 from Game import Connect4
 from DNN import DNN
+from MCTS import MCTS
+from Node import Node
 
-dnn = DNN()
-dnn.load_state_dict(torch.load('Connect4_Agent.pth'))
+
+dnn1 = DNN()
+dnn1.load_state_dict(torch.load('best.pth'))
+
+player1_wins = 0
+player2_wins = 0
+draw = 0
+
 game = Connect4()
-state = game.init_state
+
+p1 = 10
+while p1 != 0 and p1 != 1:
+    p1 = int(input('0: Máquina juega primero; 1: Humano juega primero'))
+player1 = dnn1
+
+mcts1 = MCTS(game, player1, ngames=50)
 end = False
-player = 1
-game.plot(state)
+player = -1
+node = Node(game.init_state, 1, player)
+
+game.plot(node.state)
 
 while not end:
-    a = torch.argmax(dnn(state)[0])
-    state = game.play(state, a, player)
-    end, _ = game.check_end(state)
-    game.plot(state)
-    player *= -1
-    
-    
-    if not end:
-        a = int(input())
-        state = game.play(state, a, player)
-        end, _ = game.check_end(state)
-        game.plot(state)
-        player *= -1
+    if p1 == 0:
+        if player == -1:
+            mcts1.explore(node)
+            a = mcts1.select_action(node, 0.01)
+        elif player == 1:
+            a = int(input())
+            if node.children == []:
+                mcts1.explore(node)
+
+    if p1 == 1:
+        if player == 1:
+            mcts1.explore(node)
+            a = mcts1.select_action(node, 0.01)
+        elif player == -1:
+            a = int(input())
+            if node.children == []:
+                mcts1.explore(node)
+
+
+    print(len(node.children), a)
+    node = node.children[a]
+    player = node.player
+    game.plot(node.state)
+
+    end, w = game.check_end(node.state)
+
+    # Remove childrens:
+    node.children = []
+
+print(w)
