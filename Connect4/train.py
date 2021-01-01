01e83b9d90b902b170013eab6ab0b7212cecd788
Pablo del Mazo <delmazosevillanopablo@gmail.com>
2021-01-01 12:06:44 +0100

    Now games are generated with best current DNN

--- Connect4/train.py
+++ Connect4/train.py
@@ -16,7 +16,7 @@ import Tournament
 torch.set_num_threads(1)
 
 # Train loop:
-def self_play_game():
+def self_play_game(best_model):
     # Play games to fill replay memory
     T = 0.5
     end = False
@@ -24,7 +24,7 @@ def self_play_game():
 
     game = Connect4()
     root = Node(game.init_state, 1, player)
-    mcts = MCTS(game, dnn, ngames=NGAMES)
+    mcts = MCTS(game, best_model, ngames=NGAMES)
 
     history, winner = mcts.play(root, T)
 
@@ -45,15 +45,17 @@ REPLAY_START_SIZE = 100
 MINIBATCH = 40
 BATCH = 30
 NEPOCHS = 2000
-NGAMES = 150
-GAMES_PER_EPISODE = 10
+NGAMES = 50
+GAMES_PER_EPISODE = 100
 EPISODES = 100
 
 TOURNAMENT_FREQ = 10
 
 
 Transition = namedtuple('Transition',('state', 'policy', 'reward'))
+dnn_best = DNN()
 dnn = DNN()
+dnn.load_state_dict(dnn_best.state_dict())
 replay_memory = ReplayMemory(CAPACITY, REPLAY_START_SIZE)
 
 
@@ -63,11 +65,14 @@ trainer = Trainer(NEPOCHS, MINIBATCH, BATCH)
 
 loss_hist = []
 
-dnn.save_checkpoint(name='best.pth')
+dnn_best.save_checkpoint(name='best.pth')
 for episode in range(1,EPISODES+1):
     # Play self games to fill memory
     for igame in range(GAMES_PER_EPISODE):
-        w = self_play_game()
+        if igame % 10 == 0:
+            print('Starting {} self play game'.format(igame))
+        # Self plays use the best current policy.
+        w = self_play_game(dnn_best)
 
     # Train
     loss = trainer.train(dnn, optimizer, replay_memory)
@@ -80,6 +85,7 @@ for episode in range(1,EPISODES+1):
 
         if nwin1 > nwin2:
             # Current player is better:
+            dnn_best.load_state_dict(dnn.state_dict())
             dnn.save_checkpoint(name='best.pth')
         else:
             # Reload previous model:
