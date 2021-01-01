845238166bbdc01ac233f0f8a16d138839fe3710
Pablo del Mazo <delmazosevillanopablo@gmail.com>
2021-01-01 12:06:16 +0100

    More complex DNN

--- Connect4/DNN.py
+++ Connect4/DNN.py
@@ -5,20 +5,24 @@ import torch.nn.functional as F
 class DNN(nn.Module):
     def __init__(self):
         super(DNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 5, (5,5), padding=(2,2)) # Padding to keep same input size.
-        self.l1 = nn.Linear(210, 20)
-        self.l2 = nn.Linear(20, 7)
-        self.l3 = nn.Linear(20, 1)
+        self.conv1 = nn.Conv2d(1, 10, (5,5), padding=(2,2)) # Padding to keep same input size.
+        self.conv2 = nn.Conv2d(10, 30, (5,5), padding=(2,2))
+        self.l1 = nn.Linear(1260, 150)
+        self.l2 = nn.Linear(150, 20)
+        self.l3 = nn.Linear(20, 7)
+        self.l4 = nn.Linear(20, 1)
         self.flatten = nn.Flatten()
         self.sm = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = F.relu(self.conv1(x.view(-1, 1, 6, 7)))
+        x = F.relu(self.conv2(x))
         x = self.flatten(x)
         x = F.relu(self.l1(x))
-        x1 = self.l2(x) # Logit value of policy
+        x = F.relu(self.l2(x))
+        x1 = self.l3(x) # Logit value of policy
         x1 = self.sm(x1)
-        x2 = torch.tanh(self.l3(x)) # Value head.
+        x2 = torch.tanh(self.l4(x)) # Value head.
 
         if x1.size(0)==1:
             return x1[0,:], x2[0,:]
