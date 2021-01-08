270dc08667eca79ffa26adf3e19bad6960d0b172
Pablo del Mazo <delmazosevillanopablo@gmail.com>
2021-01-08 12:20:21 +0100

    New net for connect4

--- Connect4/DNN.py
+++ Connect4/DNN.py
@@ -4,19 +4,18 @@ import torch.nn.functional as F
 
 class DNN(nn.Module):
     def __init__(self):
+        # Net structure from https://codebox.net/pages/connect4
         super(DNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, (5,5), padding=(2,2)) # Padding to keep same input size.
-        self.conv2 = nn.Conv2d(10, 30, (5,5), padding=(2,2))
-        self.l1 = nn.Linear(1260, 150)
-        self.l2 = nn.Linear(150, 20)
-        self.l3 = nn.Linear(20, 7)
-        self.l4 = nn.Linear(20, 1)
+        self.conv1 = nn.Conv2d(1, 128, (4,4)) # Padding to keep same input size.
+        self.l1 = nn.Linear(1536, 64)
+        self.l2 = nn.Linear(64, 64)
+        self.l3 = nn.Linear(64, 7)
+        self.l4 = nn.Linear(64, 1)
         self.flatten = nn.Flatten()
         self.sm = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = F.relu(self.conv1(x.view(-1, 1, 6, 7)))
-        x = F.relu(self.conv2(x))
         x = self.flatten(x)
         x = F.relu(self.l1(x))
         x = F.relu(self.l2(x))
